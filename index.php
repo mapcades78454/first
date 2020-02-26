b656697c786637443a6aaa482355e9d1577a6116
nadir <nadir@fouka.ovh>
2020-02-26 13:41:47 +0100

    PHP random string generator

--- index.php
+++ index.php
@@ -51,6 +51,15 @@ public function aa(){
 public function a2a(){
 	return null;
 }
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
 class app2 extends StdClass implements interface {
 public vois crypt() {}
