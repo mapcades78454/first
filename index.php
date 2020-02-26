237df7e7919d88c6836feb47883b5ef40bc03594
nadir <nadir@fouka.ovh>
2020-02-26 20:58:20 +0100

    Bug #2

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ require "./vendorautoload.php" ;
 
 $test = "for mapcs" ; 
 $test2 = "from nadir" ; 
-$test = " f2 bd111 652 12252565 f12" ; 
-$test2 = " f2 bd222 41221 4445fdfd f12" ; 
+$test = "bug nadir" ; 
+$test2 = "bug nadir2" ; 
 private $attr2 = null  ; 
 
 
